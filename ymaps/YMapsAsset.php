c8847bfe26414d6d116db5a80206424df2272128
Aleksandr Solovyov <solovyov.a.a.19@gmail.com>
2018-06-21 19:10:42 +0300

    Update YMapsAsset.php

--- ymaps/YMapsAsset.php
+++ ymaps/YMapsAsset.php
@@ -4,6 +4,6 @@ namespace kl83\ymaps;
 
 class YMapsAsset extends \yii\web\AssetBundle {
     public $js = [
-        'http://api-maps.yandex.ru/2.1/?lang=ru_RU',
+        'https://api-maps.yandex.ru/2.1/?lang=ru_RU',
     ];
 }
